4473352d8ea33177ccebe83bf765c01ec0092d39
rodrigonunesvr <rodrigonunes86@yahoo.com.br>
2026-02-10 22:17:16 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import Layout from './components/Layout'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import Entry from './pages/Entry'
-import Exit from './pages/Exit'
-import Admin from './pages/Admin'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Layout from './components/Layout';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Entry from './pages/Entry';
+import Exit from './pages/Exit';
+import Admin from './pages/Admin';
 
 export default function App() {
-    return (
-        <AuthProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/login\" element={<Login />} />
-                    <Route element={<Layout />}>
-                        <Route path=\"/\" element={<Home />} />
-                        <Route path=\"/entry\" element={<Entry />} />
-                        <Route path=\"/exit\" element={<Exit />} />
-                        <Route path=\"/admin\" element={<Admin />} />
-                    </Route>
-                    <Route path=\"*\" element={<Navigate to=\"/\" replace />} />
-                </Routes>
-            </BrowserRouter>
-        </AuthProvider>
-    )
-}
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
 
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/entry" element={<Entry />} />
+            <Route path="/exit" element={<Exit />} />
+            <Route path="/admin" element={<Admin />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
